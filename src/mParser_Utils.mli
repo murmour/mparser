71494dd67a321ad818e86948bf80999749fa6478
Max Mouratov <mmouratov@gmail.com>
2020-06-25 03:55:00 +0500

    Docstring tweaks

--- src/mParser_Utils.mli
+++ src/mParser_Utils.mli
@@ -27,9 +27,8 @@ module IO: sig
       channel [chn] and stores them in the byte-buffer [b], starting at position
       [pos]. It returns the actual number of characters read. A value less
       than [length] is only returned if there are less than [length] characters
-      available from [chn] (the [input] function in the [Pervasives] module is
-      allowed to read less than [length] characters if it "finds it convenient
-      to do a partial read").
+      available from [chn] ([Pervasives.input] is allowed to read less than
+      [length] characters if it "finds it convenient to do a partial read").
 
       @raise Invalid_argument if [pos] and [length] do not specify a valid
       substring of [b]. *)
@@ -41,7 +40,7 @@ module String: sig
   include module type of String
 
   val unique: string list -> string list
-  (** Returns the sorted list of unique elements in the list of strings [l] *)
+  (** [unique l] returns the sorted list of unique strings in [l]. *)
 
   val for_all: (char -> bool) -> string -> bool
   (** [for_all p s] returns [true] if [p c = true] for all characters [c] of
@@ -54,17 +53,14 @@ module Bytes: sig
   include module type of Bytes
 
   val match_sub: Bytes.t -> int -> string -> bool
-  (** [match_sub b start pat] returns [true] if the byte-buffer [b] contains
-      the string [pat] as a substring starting at position [start], and
-      [false] otherwise.
+  (** [match_sub b i s] equals [Bytes.sub b i (String.length s) = s].
 
-      @raise Invalid_argument if [start] is no valid index in [b]. *)
+      @raise Invalid_argument if [i] isn't a valid index in [b]. *)
 
   val match_sub2: Bytes.t -> int -> string -> int -> int -> bool
-  (** [match_sub2 b1 start1 s2 start2 len] returns [true] if [Bytes.sub b1
-      start1 len = String.sub s2 start2 len], and [false] otherwise.
+  (** [match_sub2 b i s j len] equals [Bytes.sub b i len = String.sub s j len].
 
-      @raise Invalid_argument if [start1], [start2], and [len] do not specify
-      valid substrings of [b1] and [s2]. *)
+      @raise Invalid_argument if [i], [j], [len] do not specify valid
+      substrings of [b] and [s]. *)
 
 end
